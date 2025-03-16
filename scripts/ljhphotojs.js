d05faa5bad76a33fc121f0c3c8e74717d02a3cfc
ljhilgenhold <ljhilgenhold@gmail.com>
2025-03-15 20:30:20 -0400

    Commit files and structure ORIGINAL

---
+++ scripts/ljhphotojs.js
@@ -0,0 +1,36 @@
+        //Nav JavaScript
+            function openNav() {
+                document.getElementById("navigationpanel").style.width = "250px";
+            }
+            function closeNav() {
+            document.getElementById("navigationpanel").style.width = "0";
+            }
+        //Slideshow JavaScript
+            let slideIndex = 1;
+            showSlides(slideIndex);
+
+            // Next/previous controls
+            function plusSlides(n) {
+            showSlides(slideIndex += n);
+            }
+
+            // Thumbnail image controls
+            function currentSlide(n) {
+            showSlides(slideIndex = n);
+            }
+
+            function showSlides(n) {
+            let i;
+            let slides = document.getElementsByClassName("slideimages");
+            let dots = document.getElementsByClassName("dot");
+            if (n > slides.length) {slideIndex = 1}
+            if (n < 1) {slideIndex = slides.length}
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex-1].style.display = "block";
+            dots[slideIndex-1].className += " active";
+            }
