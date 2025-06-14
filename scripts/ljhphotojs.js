607a784da1946e3317bc3b04237b00df6e0ab60b
ljhilgenhold <ljhilgenhold@gmail.com>
2025-06-14 16:08:26 -0400

    lots of website updating: new images slideshow auto services page portfolio page

--- scripts/ljhphotojs.js
+++ scripts/ljhphotojs.js
@@ -19,20 +19,23 @@
             showSlides(slideIndex = n);
             }
 
+
+            //Image slides
             function showSlides(n) {
             let i;
             let slides = document.getElementsByClassName("slideimages");
             let dots = document.getElementsByClassName("dot");
-            if (n > slides.length) {slideIndex = 1}
-            if (n < 1) {slideIndex = slides.length}
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
+             for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
             }
+            slideIndex++;
+            if (slideIndex > slides.length) {slideIndex = 1}    
             for (i = 0; i < dots.length; i++) {
                 dots[i].className = dots[i].className.replace(" active", "");
             }
-            slides[slideIndex-1].style.display = "block";
+            slides[slideIndex-1].style.display = "block";  
             dots[slideIndex-1].className += " active";
+            setTimeout(showSlides, 5000); // Change image every 5 seconds
             }
 
 
