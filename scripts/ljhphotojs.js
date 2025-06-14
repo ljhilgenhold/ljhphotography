6daa82720c3c1cb6c40446b5af8e18286eb18a5c
ljhilgenhold <ljhilgenhold@gmail.com>
2025-06-14 16:33:47 -0400

    fixed slideshow dots timing and remove arrows

--- scripts/ljhphotojs.js
+++ scripts/ljhphotojs.js
@@ -21,6 +21,7 @@
 
 
             //Image slides
+        
             function showSlides(n) {
             let i;
             let slides = document.getElementsByClassName("slideimages");
@@ -28,13 +29,15 @@
              for (i = 0; i < slides.length; i++) {
             slides[i].style.display = "none";  
             }
-            slideIndex++;
-            if (slideIndex > slides.length) {slideIndex = 1}    
+            if (slideIndex > slides.length) {slideIndex = 1} 
+            if (slideIndex < 1) {slideIndex = slides.length}   
             for (i = 0; i < dots.length; i++) {
                 dots[i].className = dots[i].className.replace(" active", "");
             }
             slides[slideIndex-1].style.display = "block";  
             dots[slideIndex-1].className += " active";
+
+            slideIndex++;
             setTimeout(showSlides, 5000); // Change image every 5 seconds
             }
 
