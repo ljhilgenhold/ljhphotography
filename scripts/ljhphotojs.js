4ea3677df8461ec8eb5eb8d7efca01c5f9ac9dfd
ljhilgenhold <ljhilgenhold@gmail.com>
2025-06-15 09:55:01 -0400

    fix auto slideshow with manual functions

--- scripts/ljhphotojs.js
+++ scripts/ljhphotojs.js
@@ -5,41 +5,47 @@
             function closeNav() {
             document.getElementById("navigationpanel").style.width = "0";
             }
+
         //Slideshow JavaScript
-            let slideIndex = 1;
-            showSlides(slideIndex);
+           (function () {
+  let slideIndex = 1;
+  let slideTimer;
 
-            // // Next/previous controls
-            // function plusSlides(n) {
-            // showSlides(slideIndex += n);
-            // }
+  showSlides(slideIndex);
 
-            // Thumbnail image controls
-            function currentSlide(n) {
-            showSlides(slideIndex = n);
-            }
+  // Expose these globally so HTML can call them
+  window.plusSlides = function(n) {
+    clearTimeout(slideTimer);
+    showSlides(slideIndex += n);
+  }
 
+  window.currentSlide = function(n) {
+    clearTimeout(slideTimer);
+    showSlides(slideIndex = n);
+  }
 
-            //Image slides
-        
-            function showSlides(n) {
-            let i;
-            let slides = document.getElementsByClassName("slideimages");
-            let dots = document.getElementsByClassName("dot");
-             for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";  
-            }
-            if (slideIndex > slides.length) {slideIndex = 1} 
-            if (slideIndex < 1) {slideIndex = slides.length}   
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[slideIndex-1].style.display = "block";  
-            dots[slideIndex-1].className += " active";
+  function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName("slideimages");
+    const dots = document.getElementsByClassName("dot");
 
-            slideIndex++;
-            setTimeout(showSlides, 5000); // Change image every 5 seconds
-            }
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+
+    slideTimer = setTimeout(() => showSlides(++slideIndex), 5000);
+  }
+})();
 
 
 
